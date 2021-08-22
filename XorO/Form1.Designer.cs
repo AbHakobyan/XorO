5b72fbac2a3112c17757f7f533657666d556448c
oxotn <oxotn@OXOTNIK>
2021-08-22 13:35:28 +0400

    Добавьте файлы проекта.

---
+++ XorO/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿
+namespace XorO
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btA1 = new System.Windows.Forms.Button();
+            this.btA2 = new System.Windows.Forms.Button();
+            this.btA3 = new System.Windows.Forms.Button();
+            this.btB3 = new System.Windows.Forms.Button();
+            this.btB2 = new System.Windows.Forms.Button();
+            this.btB1 = new System.Windows.Forms.Button();
+            this.btC3 = new System.Windows.Forms.Button();
+            this.btC2 = new System.Windows.Forms.Button();
+            this.btC1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutUsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.versionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btA1
+            // 
+            resources.ApplyResources(this.btA1, "btA1");
+            this.btA1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btA1.ForeColor = System.Drawing.Color.Black;
+            this.btA1.Name = "btA1";
+            this.btA1.UseVisualStyleBackColor = false;
+            this.btA1.Click += new System.EventHandler(this.btA1_Click);
+            this.btA1.MouseEnter += new System.EventHandler(this.btA1_MouseEnter);
+            this.btA1.MouseLeave += new System.EventHandler(this.btA1_MouseLeave);
+            // 
+            // btA2
+            // 
+            resources.ApplyResources(this.btA2, "btA2");
+            this.btA2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btA2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btA2.Name = "btA2";
+            this.btA2.UseVisualStyleBackColor = false;
+            this.btA2.Click += new System.EventHandler(this.btA1_Click);
+            this.btA2.MouseEnter += new System.EventHandler(this.btA1_MouseEnter);
+            this.btA2.MouseLeave += new System.EventHandler(this.btA1_MouseLeave);
+            // 
+            // btA3
+            // 
+            resources.ApplyResources(this.btA3, "btA3");
+            this.btA3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btA3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btA3.Name = "btA3";
+            this.btA3.UseVisualStyleBackColor = false;
+            this.btA3.Click += new System.EventHandler(this.btA1_Click);
+            this.btA3.MouseEnter += new System.EventHandler(this.btA1_MouseEnter);
+            this.btA3.MouseLeave += new System.EventHandler(this.btA1_MouseLeave);
+            // 
+            // btB3
+            // 
+            resources.ApplyResources(this.btB3, "btB3");
+            this.btB3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btB3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btB3.Name = "btB3";
+            this.btB3.UseVisualStyleBackColor = false;
+            this.btB3.Click += new System.EventHandler(this.btA1_Click);
+            this.btB3.MouseEnter += new System.EventHandler(this.btA1_MouseEnter);
+            this.btB3.MouseLeave += new System.EventHandler(this.btA1_MouseLeave);
+            // 
+            // btB2
+            // 
+            resources.ApplyResources(this.btB2, "btB2");
+            this.btB2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btB2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btB2.Name = "btB2";
+            this.btB2.UseVisualStyleBackColor = false;
+            this.btB2.Click += new System.EventHandler(this.btA1_Click);
+            this.btB2.MouseEnter += new System.EventHandler(this.btA1_MouseEnter);
+            this.btB2.MouseLeave += new System.EventHandler(this.btA1_MouseLeave);
+            // 
+            // btB1
+            // 
+            resources.ApplyResources(this.btB1, "btB1");
+            this.btB1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btB1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btB1.Name = "btB1";
+            this.btB1.UseVisualStyleBackColor = false;
+            this.btB1.Click += new System.EventHandler(this.btA1_Click);
+            this.btB1.MouseEnter += new System.EventHandler(this.btA1_MouseEnter);
+            this.btB1.MouseLeave += new System.EventHandler(this.btA1_MouseLeave);
+            // 
+            // btC3
+            // 
+            resources.ApplyResources(this.btC3, "btC3");
+            this.btC3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btC3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btC3.Name = "btC3";
+            this.btC3.UseVisualStyleBackColor = false;
+            this.btC3.Click += new System.EventHandler(this.btA1_Click);
+            this.btC3.MouseEnter += new System.EventHandler(this.btA1_MouseEnter);
+            this.btC3.MouseLeave += new System.EventHandler(this.btA1_MouseLeave);
+            // 
+            // btC2
+            // 
+            resources.ApplyResources(this.btC2, "btC2");
+            this.btC2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btC2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btC2.Name = "btC2";
+            this.btC2.UseVisualStyleBackColor = false;
+            this.btC2.Click += new System.EventHandler(this.btA1_Click);
+            this.btC2.MouseEnter += new System.EventHandler(this.btA1_MouseEnter);
+            this.btC2.MouseLeave += new System.EventHandler(this.btA1_MouseLeave);
+            // 
+            // btC1
+            // 
+            resources.ApplyResources(this.btC1, "btC1");
+            this.btC1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btC1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btC1.Name = "btC1";
+            this.btC1.UseVisualStyleBackColor = false;
+            this.btC1.Click += new System.EventHandler(this.btA1_Click);
+            this.btC1.MouseEnter += new System.EventHandler(this.btA1_MouseEnter);
+            this.btC1.MouseLeave += new System.EventHandler(this.btA1_MouseLeave);
+            // 
+            // listView1
+            // 
+            resources.ApplyResources(this.listView1, "listView1");
+            this.listView1.Name = "listView1";
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // menuStrip1
+            // 
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Name = "menuStrip1";
+            // 
+            // gameToolStripMenuItem
+            // 
+            resources.ApplyResources(this.gameToolStripMenuItem, "gameToolStripMenuItem");
+            this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            resources.ApplyResources(this.newGameToolStripMenuItem, "newGameToolStripMenuItem");
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            resources.ApplyResources(this.exitToolStripMenuItem, "exitToolStripMenuItem");
+            this.exitToolStripMenuItem.BackColor = System.Drawing.SystemColors.Control;
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            resources.ApplyResources(this.helpToolStripMenuItem, "helpToolStripMenuItem");
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutUsToolStripMenuItem,
+            this.versionToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            // 
+            // aboutUsToolStripMenuItem
+            // 
+            resources.ApplyResources(this.aboutUsToolStripMenuItem, "aboutUsToolStripMenuItem");
+            this.aboutUsToolStripMenuItem.Name = "aboutUsToolStripMenuItem";
+            this.aboutUsToolStripMenuItem.Click += new System.EventHandler(this.aboutUsToolStripMenuItem_Click);
+            // 
+            // versionToolStripMenuItem
+            // 
+            resources.ApplyResources(this.versionToolStripMenuItem, "versionToolStripMenuItem");
+            this.versionToolStripMenuItem.Name = "versionToolStripMenuItem";
+            this.versionToolStripMenuItem.Click += new System.EventHandler(this.versionToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btC3);
+            this.Controls.Add(this.btC2);
+            this.Controls.Add(this.btC1);
+            this.Controls.Add(this.btB3);
+            this.Controls.Add(this.btB2);
+            this.Controls.Add(this.btB1);
+            this.Controls.Add(this.btA3);
+            this.Controls.Add(this.btA2);
+            this.Controls.Add(this.btA1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.TransparencyKey = System.Drawing.Color.DimGray;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btA1;
+        private System.Windows.Forms.Button btA2;
+        private System.Windows.Forms.Button btA3;
+        private System.Windows.Forms.Button btB3;
+        private System.Windows.Forms.Button btB2;
+        private System.Windows.Forms.Button btB1;
+        private System.Windows.Forms.Button btC3;
+        private System.Windows.Forms.Button btC2;
+        private System.Windows.Forms.Button btC1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutUsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem versionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ XorO/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XorO
+{
+
+    public partial class Form1 : Form
+    {
+        public bool push = true;
+        public int pushCount = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btA1_Click(object sender, EventArgs e)
+        {
+            Button bt = (Button)sender;
+            if (push)
+            {
+                bt.Text = "X";
+                bt.ForeColor = Color.Red;
+            }
+            else
+            {
+                bt.Text = "O";
+                bt.ForeColor = Color.DarkBlue;
+            }
+            push = !push;
+            bt.Enabled = false;
+            pushCount++;
+            CheckWinner();
+        }
+
+        private void CheckWinner()
+        {
+            if (btA1.Text == "X" && btA2.Text == "X" && btA3.Text == "X")
+            {
+                StartNewGame();
+                label6.Text = (int.Parse(label6.Text) + 1).ToString();
+            }
+            else if(btA1.Text == "X" && btB2.Text == "X" && btC3.Text == "X")
+            {
+                StartNewGame();
+                label6.Text = (int.Parse(label6.Text) + 1).ToString();
+            }
+            else if(btC1.Text == "X" && btB2.Text == "X" && btA3.Text == "X")
+            {
+                StartNewGame();
+                label6.Text = (int.Parse(label6.Text) + 1).ToString();
+            }
+            else if(btC1.Text == "X" && btC2.Text == "X" && btC3.Text == "X")
+            {
+                StartNewGame();
+                label6.Text = (int.Parse(label6.Text) + 1).ToString();
+            }
+            else if(btC1.Text == "X" && btB1.Text == "X" && btA1.Text == "X")
+            {
+                StartNewGame();
+                label6.Text = (int.Parse(label6.Text) + 1).ToString();
+            }
+            else if(btC3.Text == "X" && btB3.Text == "X" && btA3.Text == "X")
+            {
+                StartNewGame();
+                label6.Text = (int.Parse(label6.Text) + 1).ToString();
+            }
+            else if(btB1.Text == "X" && btB2.Text == "X" && btB3.Text == "X")
+            {
+                StartNewGame();
+                label6.Text = (int.Parse(label6.Text) + 1).ToString();
+            }
+            if (btA1.Text == "O" && btA2.Text == "O" && btA3.Text == "O")
+            {
+                StartNewGame();
+                label5.Text = (int.Parse(label5.Text) + 1).ToString();
+            }
+            else if (btA1.Text == "O" && btB2.Text == "O" && btC3.Text == "O")
+            {
+                StartNewGame();
+                label5.Text = (int.Parse(label5.Text) + 1).ToString();
+            }
+            else if (btC1.Text == "O" && btB2.Text == "O" && btA3.Text == "O")
+            {
+                StartNewGame();
+                label5.Text = (int.Parse(label5.Text) + 1).ToString();
+            }
+            else if (btC1.Text == "O" && btC2.Text == "O" && btC3.Text == "O")
+            {
+                StartNewGame();
+                label5.Text = (int.Parse(label5.Text) + 1).ToString();
+            }
+            else if (btC1.Text == "O" && btB1.Text == "O" && btA1.Text == "O")
+            {
+                StartNewGame();
+                label5.Text = (int.Parse(label5.Text) + 1).ToString();
+            }
+            else if (btC3.Text == "O" && btB3.Text == "O" && btA3.Text == "O")
+            {
+                StartNewGame();
+                label5.Text = (int.Parse(label5.Text) + 1).ToString();
+            }
+            else if (btB1.Text == "O" && btB2.Text == "O" && btB3.Text == "O")
+            {
+                StartNewGame();
+                label5.Text = (int.Parse(label5.Text) + 1).ToString();
+            }
+            if (pushCount == 9)
+            {
+                MessageBox.Show("We don't have any winner", "XorO", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                StartNewGame();
+                label4.Text = (int.Parse(label4.Text) + 1).ToString();
+            }
+            if (label6.Text == "3" || label5.Text == "3")
+            {
+                label6.Text = "0";
+                label5.Text = "0";
+                label4.Text = "0";
+            }
+        }
+
+        private void StartNewGame()
+        {
+            pushCount = 0;
+            push = true;
+
+            foreach (Control item in Controls)
+            {
+                try
+                {
+                    Button bt = (Button)item;
+                    bt.Enabled = true;
+                    bt.Text = "";
+                }
+                catch
+                {
+
+                }
+            }
+        }
+        private void btA1_MouseLeave(object sender, EventArgs e)
+        {
+            Button bt = (Button)sender;
+
+            if (bt.Enabled)
+            {
+                bt.Text = "";
+            }
+        }
+        private void btA1_MouseEnter(object sender, EventArgs e)
+        {
+            Button bt = (Button)sender;
+            if (bt.Enabled)
+            {
+                if (push)
+                {
+                    bt.Text = "X";
+                    bt.ForeColor = Color.Red;
+                }
+                else
+                {
+                    bt.Text = "O";
+                    bt.ForeColor = Color.DarkBlue;
+                }
+            }
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            StartNewGame();
+        }
+
+        private void aboutUsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This Program created Albert Hakobyan For Azniv Mheryan with love");
+        }
+
+        private void versionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Version 1.0");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBoxButtons msb = MessageBoxButtons.YesNo;
+            string message = "Are you sure to Exit";
+            string caption = "Exiit";
+            if(MessageBox.Show(message,caption,msb) == DialogResult.Yes)
+            this.Close();
+        }
+    }
+}
